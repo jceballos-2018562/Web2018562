68d7239c19d3ed8edcff1caf6430307f14e8365b
Jose Ceballos <jceballos-2018562@kinal.edu.gt>
2023-01-20 08:59:13 -0600

    Primer commit v1 web

---
+++ js/index.js
@@ -0,0 +1,9 @@
+
+function sumar(){
+    let num1 = document.getElementById('numero1').value;
+    let num2 = document.getElementById('numero2').value;
+
+    let resultado = parseInt(num1) + parseInt(num2);
+    console.log('sumar')
+    console.log(resultado);
+}
